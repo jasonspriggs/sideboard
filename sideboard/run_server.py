ceff93deec8c86c7574914ebfa977f3126be61ab
Mark Murnane <mark@hackafe.net>
2019-06-27 23:48:12 -0400

    Update run_server.py

--- sideboard/run_server.py
+++ sideboard/run_server.py
@@ -5,5 +5,12 @@ import cherrypy
 import sideboard.server
 
 if __name__ == '__main__':
+    app_conf = {}
+    cherrypy.config.update({
+        'global': {
+             'engine.autoreload.on' : False
+         }
+    })
+    cherrypy.tree.mount(root=None, config=app_conf)
     cherrypy.engine.start()
     cherrypy.engine.block()
